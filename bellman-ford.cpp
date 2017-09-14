cabe9328052e573dc2b62ea0c2654d45cb48da1b
hamdani <hamdani.inbox@gmail.com>
2017-09-14 20:46:43 +0700

    Construct build_matrix procedure

--- bellman-ford.cpp
+++ bellman-ford.cpp
@@ -14,6 +14,36 @@ void build_matrix(int size, int data);
 float bellman_ford(int size);
 void print_path(int par[], int child);
 
+void build_matrix(int size, int data) {
+
+  // Local variable declaration
+  ifstream graph;
+  int i, j;
+
+  // Open the txt data in the same directory
+  // Make sure that you write the correct file name
+  if (data == 1) {
+    graph.open("graf1.txt");
+    if (!graph) {
+      cout << "Warning! graf1.txt doesn't exist";
+    }
+  } else if (data == 2) {
+    graph.open("graf2.txt");
+    if (!graph) {
+      cout << "Warning! graf2.txt doesn't exist";
+    }
+  }
+
+  // Input the data in the file into the matrix distance
+  for (i = 0; i <size; i++) {
+    for (j = 0; j < size; j++) {
+      graph >> matrix[i][j];
+    }
+    cout << endl;
+  }
+
+}
+
 int main(){
 
   // Varible declaration
@@ -31,6 +61,9 @@ int main(){
     cout << "Your option : ";
     cin >> option;
   } while(option < 1 or option > 2);
+
+  // Call build_matrix function
+  build_matrix(vertice, option);
 
   return 0;
 }
