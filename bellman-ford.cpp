52372f89aa312bf769ce8bdc473cb1d5f1fcbdaf
Syahrul B Hamdani <hamdani.inbox@gmail.com>
2017-09-14 17:57:21 +0700

    Remove switch case part

--- bellman-ford.cpp
+++ bellman-ford.cpp
@@ -27,13 +27,9 @@ int main(){
   cout << "List of data for input: " << endl;
   cout << "1. Simple 10 vertices graph" << endl;
   cout << "2. Improved 10 vertices graph with negative weight" << endl;
-  cout << "Your option : ";
-  cin >> option;
   do {
-    switch (option) {
-      default :
-        cout << "Undefined. Please try again" << endl;
-    }
+    cout << "Your option : ";
+    cin >> option;
   } while(option < 1 or option > 2);
 
   return 0;
